37ffe4765a13759cb37e597943e1adee8c40fd25
Bergqvist1 <111962893+Bergqvist1@users.noreply.github.com>
2024-05-29 10:27:20 +0200

    Update Fiende.cs

--- Fiende.cs
+++ Fiende.cs
@@ -8,9 +8,9 @@ using System.Reflection.Metadata.Ecma335;
 namespace OOP_Prov
 {
 
-    public class Fiende: Hjälte
+    public class Fiende: Hjälte // bas klassen för fiender som ärver av klassen Hjälte 
     {
-        public Fiende(int hp, string typ, int Minstskada, int MestSkada): base(hp, typ, Minstskada, MestSkada){}
+        public Fiende(int hp, string typ, int Minstskada, int MestSkada): base(hp, typ, Minstskada, MestSkada){} // Gör så att fienden kan sätta hp, typ, MinstSkada och MestSkada
     }    
 
 
