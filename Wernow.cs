b78c88266a532aa17f37629014c1e24a755f6b96
Bergqvist1 <111962893+Bergqvist1@users.noreply.github.com>
2024-05-29 10:29:43 +0200

    Update Wernow.cs

--- Wernow.cs
+++ Wernow.cs
@@ -8,9 +8,9 @@ using System.Reflection.Metadata.Ecma335;
 namespace OOP_Prov
 {
     
-    public class Wernow: Fiende
+    public class Wernow: Fiende // Skapar fiende klasen Wernow som ärver av Fiende
     {
-        public Wernow(): base(60, "Wernow", 4,15){}
+        public Wernow(): base(60, "Wernow", 4,15){}  // sätter fiende klassen Wernows info utifån fiende
     }
 }
 
